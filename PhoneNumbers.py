30f0274adc79ee914f78dfaba9e4372c79123371
Arun Kumar <arun89@terpmail.umd.edu>
2020-05-01 19:47:22 -0400

    Added code for finding valid english words

--- PhoneNumbers.py
+++ PhoneNumbers.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+import pprint
+import enchant
+import time
 
 
 class AlphaNumeric:
-    def __init__(self, lib_location='words.txt'):
+    def __init__(self, phone_number='9883642228'):
         self.num_alpha = {"1": ["NIL"], "2": ["A", "B", "C"],
                           "3": ["D", "E", "F"], "4": ["G", "H", "I"],
                           "5": ["J", "K", "L"], "6": ["M", "N", "O"],
@@ -14,16 +17,16 @@ class AlphaNumeric:
                           "S": "7", "T": "8", "U": "8", "V": "8", "W": "9", "X": "9",
                           "Y": "9", "Z": "9"}
         self.comb_dict = {}
-        with open(lib_location, 'r') as lib_file:
-            self.words = lib_file.read().splitlines()
+        self.valid_numbers = {}
+        self.words = enchant.Dict("en_US")
 
-    def find_permutations_num_alpha(self, number='9880042436'):
+    def find_permutations_num_alpha(self, number='9883642228'):
         counter = 0
         interim_combs = []
         if self.num_alpha[number[0]] is "NIL":
             interim_combs.append(number[0])
         else:
-            for each_alpha in self.num_alpha[number[0][:]]:
+            for each_alpha in self.num_alpha[number[0]]:
                 interim_combs.append(each_alpha)
         self.comb_dict[counter] = tuple(interim_combs)
         while counter < len(number) - 1:
@@ -52,8 +55,29 @@ class AlphaNumeric:
                 converted_num += each_char
         return converted_num
 
+    def accumulate_valid_numbers(self, num_range=(6, 10), original_num='9883642228'):
+        temp_set = set()
+        for each_word in self.comb_dict[9]:
+            temp_set.add(str(each_word[num_range[0]: num_range[1]]).lower())
+        unique_set = set()
+        for unique_name in temp_set:
+            if self.words.check(unique_name):
+                unique_set.add(original_num[0:num_range[0]] + unique_name.upper() + original_num[num_range[1]:])
+            self.valid_numbers[num_range] = unique_set
+
+    def print_all_combinations(self):
+        pprint.pprint(self.valid_numbers)
+
+    def get_all_num_to_text(self):
+        return self.valid_numbers
+
 
 if "__main__":
+    start = time.time()
     NumAlpha = AlphaNumeric()
-    print(NumAlpha.find_permutations_num_alpha())
-    print(NumAlpha.convert_alpha_to_num())
+    NumAlpha.find_permutations_num_alpha()
+    NumAlpha.convert_alpha_to_num()
+    NumAlpha.accumulate_valid_numbers()
+    end = time.time()
+    NumAlpha.print_all_combinations()
+    print("Total time taken : ", end - start)

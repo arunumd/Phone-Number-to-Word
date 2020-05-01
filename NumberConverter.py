3474f6201dccd51e07dc6a60acfb3649a0255a22
Arun Kumar <arun89@terpmail.umd.edu>
2020-05-01 02:40:55 -0400

    Created main script with helper functions

---
+++ NumberConverter.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import re
+import time
+import pprint
+import logging
+import argparse
+import threading
+from itertools import combinations
+
+from PhoneNumbers import AlphaNumeric
+
+parser = argparse.ArgumentParser(description='A tool for conversion between Phone Number and Text')
+parser.add_argument('--mode', action="store", dest="mode",
+                    default="num_to_text", help="Specify the action here.\n" "Example : \n"
+                                                "num_to_text\n" "text_to_num\n" "num_to_all_text\n")
+parser.add_argument('--log', action="store", dest="log", default="output.txt", help="Specify the action here.")
+options = parser.parse_args()
+
+
+def convert_to_alpha_num(number='984ABC&/]?786'):
+    return ''.join([c for c in number if c.isalnum()])
+
+
+def is_all_num(number='989592KJABGV8'):
+    return number.isdigit()
+
+
+def check_format(number='1989592KJAB'):
+    return number[0] == '1' and len(number) == 11
+
+
+def fracture_number(number='989592KJAB'):
+    length = len(number) + 1
+    return [[x, y] for x, y in combinations(range(length), r=2)]
+
+
+def get_good_prospects(combs=None):
+    if combs is None:
+        combs = ([1, 2], [5, 6])
+    good_prospects = []
+    for each_interval in combs:
+        if each_interval[1] - each_interval[0] > 2:
+            good_prospects.append(each_interval)
+    return good_prospects
+
+
+if '__main__':
+    print(convert_to_alpha_num())
+    print(check_format())
+    print("The number of combinations is : ", len(fracture_number()))
+    print("The no. of workers is : ", len(get_good_prospects(fracture_number())))

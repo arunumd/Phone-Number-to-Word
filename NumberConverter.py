c5f0fe8099c219d4f5f7b75b62f758b2a91fc7d3
Arun Kumar <arun89@terpmail.umd.edu>
2020-05-06 23:25:22 -0400

    Pruned the code and added docstrings documentation

--- NumberConverter.py
+++ NumberConverter.py
@@ -1,46 +1,71 @@
 #!/usr/bin/env python3
-
 import re
 import time
-import pprint
+import random
 import logging
-import argparse
 import threading
+import concurrent.futures
 from itertools import combinations
 
 from PhoneNumbers import AlphaNumeric
 
-parser = argparse.ArgumentParser(description='A tool for conversion between Phone Number and Text')
-parser.add_argument('--mode', action="store", dest="mode",
-                    default="num_to_text", help="Specify the action here.\n" "Example : \n"
-                                                "num_to_text\n" "text_to_num\n" "num_to_all_text\n")
-parser.add_argument('--log', action="store", dest="log", default="output.txt", help="Specify the action here.")
-options = parser.parse_args()
-
 
 def convert_to_alpha_num(number='984ABC&/]?786'):
+    """
+    Description
+    -----------
+    Helper function for removing characters that are neither alphabets nor numbers, from a sequence
+    :param number: Input sequence as a string
+    :return: Sequence with only digits and alphabets
+    """
     return ''.join([c for c in number if c.isalnum()])
 
 
 def is_all_num(number='989592KJABGV8'):
+    """
+    Description
+    -----------
+    Trivial function to check if an input string contains all numbers
+    :param number: Input phone number as a string
+    :return: True if all numbers, False if not
+    """
     return number.isdigit()
 
 
 def check_format(number='1989592KJAB'):
+    """
+    Description
+    -----------
+    Function to check format compliance of US phone numbers, namely the length should be exactly 11 digits, and
+    the first digit must be with 1
+    :param number: Input phone number as a string
+    :return: True if valid format, false if not
+    """
     return number[0] == '1' and len(number) == 11
 
 
-def fracture_number(number='989592KJAB'):
-    length = len(number) + 1
-    return [[x, y] for x, y in combinations(range(length), r=2)]
+def fracture_number(number='1989592KJAB'):
+    length = len(number)
+    """
+    Description
+    -----------
+    This function returns a list of slicing indices for generating sub-strings from the generated phone numbers, 
+    which will then be used to search through an English dictionary.
+    :param number: Input phone number as a string
+    :return: A list of slice indices of the format ((start1, end1), (start2, end2), ....)
+    """
+    return ((x, y) for x, y in combinations(range(length), r=2))
 
 
 def get_good_prospects(combs=([1, 2], [5, 6])):
-    return [[num[0], num[1]] for num in combs if num[1] - num[0] > 2]
+    """
+    Description
+    -----------
+    Words with just one or two letters are usually articles, prepositions, etc. Since these are not words with a
+    significant meaning, they will be omitted. Hence, this function returns a curated slice list where the distance
+    between the start point and end point of slice is at least 2 units
+    :param combs: Raw list of indices for slicing the input phone number
+    :return A curated list where the difference between end and start are at least 2 units
+    """
+    return [(num[0], num[1]) for num in combs if num[1] - num[0] > 2]
 
-
-if '__main__':
-    print(convert_to_alpha_num())
-    print(check_format())
-    print("The number of combinations is : ", len(fracture_number()))
-    print("The no. of workers is : ", len(get_good_prospects(fracture_number())))
--- PhoneNumbers.py
+++ PhoneNumbers.py
@@ -1,34 +1,42 @@
 #!/usr/bin/env python3
-import pprint
 import enchant
-import time
+from collections import deque
 
 
 class AlphaNumeric:
     def __init__(self, phone_number='9883642228'):
+        """
+        Class initializer function which sets up the basic infrastructure for the member functions like:
+        alphabet-number lookup and number-alphabet lookup tables. This function also imports a standard
+        English dictionary for valid words verification process
+        :param phone_number: Input phone number for processing by the member functions
+        """
         self.phone_number = phone_number
         self.num_alpha = {"1": ["NIL"], "2": ["A", "B", "C"],
                           "3": ["D", "E", "F"], "4": ["G", "H", "I"],
                           "5": ["J", "K", "L"], "6": ["M", "N", "O"],
                           "7": ["P", "Q", "R", "S"], "8": ["T", "U", "V"],
                           "9": ["W", "X", "Y", "Z"], "0": ["NIL"]}
-        self.alpha_num = {"A": "2", "B": "2", "C": "2", "D": "3", "E": "3", "F": "3",
-                          "G": "4", "H": "4", "I": "4", "J": "5", "K": "5", "L": "5",
-                          "M": "6", "N": "6", "O": "6", "P": "7", "Q": "7", "R": "7",
-                          "S": "7", "T": "8", "U": "8", "V": "8", "W": "9", "X": "9",
-                          "Y": "9", "Z": "9"}
+        self.alpha_num = {"A": "2", "B": "2", "C": "2", "D": "3", "E": "3",
+                          "F": "3", "G": "4", "H": "4", "I": "4", "J": "5",
+                          "K": "5", "L": "5", "M": "6", "N": "6", "O": "6",
+                          "P": "7", "Q": "7", "R": "7", "S": "7", "T": "8",
+                          "U": "8", "V": "8", "W": "9", "X": "9", "Y": "9", "Z": "9"}
         self.comb_dict = {}
-        self.valid_numbers = {}
+        self.valid_numbers = deque([], 60)
         self.words = enchant.Dict("en_US")
 
     def find_permutations_num_alpha(self):
+        """
+        Description
+        -----------
+        Function to convert an input number into all possible strings using the keypad combinations for
+        individual digits. This method uses dynamic programming (DP) to recursively solve sub-problems of
+        increasing number of digits. The results of the previous sub-problem is used as input for the next
+        sub-problem higher up in the hierarchy level. The results of every sub-problem are stored in a dictionary
+        """
         counter = 0
-        interim_combs = []
-        if self.num_alpha[self.phone_number[0]] is "NIL":
-            interim_combs.append(self.phone_number[0])
-        else:
-            for each_alpha in self.num_alpha[self.phone_number[0]]:
-                interim_combs.append(each_alpha)
+        interim_combs = [self.phone_number[0]]
         self.comb_dict[counter] = tuple(interim_combs)
         while counter < len(self.phone_number) - 1:
             interim_combs.clear()
@@ -45,9 +53,15 @@ class AlphaNumeric:
                                           each_comb + next_combs[2]))
                 self.comb_dict[counter + 1] = tuple(interim_combs)
             counter += 1
-        return self.comb_dict[9]
 
     def convert_alpha_to_num(self):
+        """
+        Description
+        -----------
+        A simple function which uses a lookup dictionary to find the corresponding numbers for given alphabets
+        in a phone number. The numbers are replaced with the alphabets and returned as an output string
+        :return: Phone number in numerical format
+        """
         converted_num = ""
         for each_char in self.phone_number:
             if each_char.isalpha():
@@ -57,6 +71,15 @@ class AlphaNumeric:
         return converted_num
 
     def accumulate_valid_numbers(self, num_range=(6, 10)):
+        """
+        Description
+        -----------
+        Function which iteratively slices the string between the given index range 'num_range' and checks whether
+        the sliced string is a valid English word by looking up in an English dictionary. If the word is a valid
+        English word, then the whole phone number is packed with the missing segments and returned
+        :param num_range: index for slicing the string into a sub-string for valid word search
+        :return: Converted phone numbers which have valid English words
+        """
         temp_set = set()
         for each_word in self.comb_dict[9]:
             temp_set.add(str(each_word[num_range[0]: num_range[1]]).lower())
@@ -65,23 +88,22 @@ class AlphaNumeric:
             if self.words.check(unique_name):
                 unique_set.add(self.phone_number[:num_range[0]] + unique_name.upper() +
                                self.phone_number[num_range[1]:])
-        if unique_set == set():
-            unique_set.add("NIL")
-        self.valid_numbers[num_range] = unique_set
+        if unique_set != set():
+            self.valid_numbers.append(unique_set)
 
     def print_all_combinations(self):
-        pprint.pprint(self.valid_numbers)
+        """
+        Description
+        -----------
+        Trivial function to print all possible phone numbers with valid English words
+        """
+        print(self.valid_numbers)
 
     def get_all_num_to_text(self):
+        """
+        Description
+        -----------
+        Trivial getter function for retrieving all possible phone numbers with valid English words
+        :return: All possible phone numbers with valid English words
+        """
         return self.valid_numbers
-
-
-if "__main__":
-    start = time.time()
-    NumAlpha = AlphaNumeric()
-    NumAlpha.find_permutations_num_alpha()
-    NumAlpha.convert_alpha_to_num()
-    NumAlpha.accumulate_valid_numbers()
-    end = time.time()
-    NumAlpha.print_all_combinations()
-    print("Total time taken : ", end - start)
